ad573c834d9d0339b0326392f0c356d9714a0a47
Syrsly <5002349+syrsly@users.noreply.github.com>
2025-11-20 09:55:27 -0500

    Added Date_Helper class and copied its single function, datetime_from_input, to validation.php as well because it's also used for validation.
    The datetime_from_input function is extremely useful when you have to support a bunch of datetime formats. PHP does not make datetime validation easy, so this is far from perfect, but it should get you most of the way to a proper solution.
    
    I still have more coming later today.

--- date.php
+++ date.php
@@ -1,19 +1,11 @@
 <?php
 
-/* Common validation tasks for PHP5+
-	You could copy-and-paste any of these standalone functions into a system class or math class or whatever is easiest for your project.
-	For example, in this validation.php script, I went ahead and made it a Validator class.
+/* Common date operations for PHP5+
+	You could copy-and-paste any of these standalone functions into a system class or date helper class or whatever is easiest for your project.
+	For example, in this date.php script, I went ahead and made it a Date_Helper class.
 	These functions are all standalone, though, and do not cross-contaminant. */
 
-class Validator {
-	/* encapsulated_by_single_quotes is a function I often use to check if a string is already encapsulated for the purpose of sanitizing strings *from* SQL.
-		You should *not* ever use this function to bypass sanitization, however. It's more a way to check for odd character placement and add extra handlers.
-		For example, you might have bad data in a table you have to pull from.
-	*/
-	public function encapsulated_by_single_quotes($string) {
-		return (strlen($string) >= 2 && $string[0] === '\'' && $string[strlen($string) - 1] === '\'');
-	}
-
+class Date_Helper {
 	/* datetime_from_input function is what I like to use when I need to validate a datetime string.
 		I just run the string into this function and get either a datetime object or if there's an error, false output. */
 	public function datetime_from_input($input_date) {
